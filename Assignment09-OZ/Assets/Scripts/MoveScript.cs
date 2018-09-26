ac2e84764117c321d39f66e4fa599b0ecc3ce5f7
Olivia <LeafeonZ.dd@gmail.com>
2018-09-26 11:00:04 -0400

    Commit 1- final

---
+++ Assignment09-OZ/Assets/Scripts/MoveScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour {
+    public int targetPnt;
+    private PathScript path;
+
+
+	// Use this for initialization
+	void Start () {
+        path = GameObject.FindGameObjectWithTag("GameController").GetComponent<PathScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, path.Points[targetPnt], Time.deltaTime*2f);
+
+    if( Vector3.Distance(transform.position,path.Points[targetPnt]) <= 0.1f){
+            targetPnt += 1;
+            if(targetPnt > path.Points.Count-1) {
+                targetPnt = 0;
+            }
+        }
+	}
+
+
+}
---
+++ Assignment09-OZ/Assets/Scripts/PathScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathScript : MonoBehaviour {
+    public List<Vector3> Points;
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1f, 0f, 0f);
+        foreach (Vector3 vector in Points)
+        {
+            Gizmos.DrawSphere(vector, 0.1f);
+        }
+        Gizmos.DrawLine(Points[0], Points[1]);
+        Gizmos.DrawLine(Points[1], Points[2]);
+        Gizmos.DrawLine(Points[2], Points[3]);
+        Gizmos.DrawLine(Points[3], Points[4]);
+        Gizmos.DrawLine(Points[4], Points[0]);
+
+
+    }
+}
